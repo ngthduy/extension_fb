ae448ede0ab69fdc90e28aafeadbb0a23f7f8e26
ngthduy <nguyenduy170898@gmail.com>
2020-08-21 16:50:06 +0700

    Realease Project

---
+++ popup.js
@@ -0,0 +1,150 @@
+var hash_cookie = {};
+var currentUrl = "";
+function loadCookie() {
+  chrome.tabs.getSelected(null, function (tab) {
+    // null defaults to current window
+    currentUrl = tab.url;
+    if (currentUrl.indexOf("chrome://newtab") > -1) {
+      currentUrl = "https://www.facebook.com";
+    }
+    $("#UrlCookieCurrent").html(extractHostname(currentUrl));
+    chrome.cookies.getAll({ url: currentUrl }, function (cookie) {
+      var result = "";
+      for (var i = 0; i < cookie.length; i++) {
+        console.log(cookie[i]);
+        result += cookie[i].name + "=" + cookie[i].value + ";\n";
+        if (cookie[i].name == "c_user" || cookie[i].name == "xs") {
+          currentUid = cookie[i].value;
+          hash_cookie[cookie[i].name] = cookie[i].value;
+        }
+      }
+      //   $.each(hash_cookie, function (key, val) {
+      //     $("table.list-cookie").append(
+      //       "<tr><td>" + key + "</td><td>" + val + "</td></tr>"
+      //     );
+      //   });
+      //   $('table.list-cookie').show();
+      chrome.tabs.getSelected(null, function (tab) {
+        chrome.tabs.executeScript(
+          tab.id,
+          {
+            code: 'localStorage["z_uuid"]',
+          },
+          function (imei) {
+            if (imei != undefined && imei != null && imei != "") {
+              result += "imei=" + imei + "; ";
+            }
+            document.getElementById("cookieResult").value =
+              result + "\n" + navigator.userAgent;
+          }
+        );
+      });
+    });
+  });
+}
+function extractHostname(url) {
+  var hostname;
+  if (url.indexOf("://") > -1) {
+    hostname = url.split("/")[2];
+  } else {
+    hostname = url.split("/")[0];
+  }
+  hostname = hostname.split(":")[0];
+  hostname = hostname.split("?")[0];
+  return hostname;
+}
+function setCookie(key, val, expiry) {
+  chrome.cookies.set(
+    {
+      url: "https://www.facebook.com",
+      name: key,
+      value: val,
+      path: "/",
+      httpOnly: false,
+      domain: ".facebook.com",
+      expirationDate: 1655213071.160959,
+    },
+    function (cookie) {
+    //   console.log(cookie);
+    }
+  );
+  chrome.tabs.getSelected(null, function (tab) {
+    var code = "window.location.reload();";
+    chrome.tabs.executeScript(tab.id, { code: code });
+  });
+  //   chrome.cookies.set({
+  //     url: "https://upload.facebook.com",
+  //     name: key,
+  //     value: val,
+  //   });
+  //   chrome.cookies.set({
+  //     url: "https://business.facebook.com",
+  //     name: key,
+  //     value: val,
+  //   });
+  //   chrome.cookies.set({
+  //     url: "https://web.facebook.com",
+  //     name: key,
+  //     value: val,
+  //   });
+  //   chrome.cookies.set({ url: "https://m.facebook.com", name: key, value: val });
+  //   chrome.cookies.set({
+  //     url: "https://mbasic.facebook.com",
+  //     name: key,
+  //     value: val,
+  //   });
+  //   chrome.cookies.set({
+  //     url: "https://developers.facebook.com",
+  //     name: key,
+  //     value: val,
+  //   });
+  //   chrome.cookies.set({
+  //     url: "https://mobile.facebook.com",
+  //     name: key,
+  //     value: val,
+  //   });
+}
+function loadObject(obj) {
+  $.each(obj, function (key, val) {
+    $("table.list-cookie tbody").append(
+      "<tr><td>" + key + "</td><td>" + val + "</td></tr>"
+    );
+    setCookie(key, val, "");
+  });
+  var count = Object.keys(obj).length;
+  $("table.list-cookie tbody").append(
+    "<tr><td colspan='2' style='text-align:left; font-weight:bold'>Insert " +
+      count +
+      " cookie succcessfully</td></tr>"
+  );
+  $("table.list-cookie").show();
+  $("table.list-cookie tbody td").css("padding", "10px");
+  loadCookie();
+}
+loadCookie();
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("btnDownloadCookie").onclick = function () {
+    now_day = new Date().toISOString().split("T")[0];
+    fileName =
+      "my-cookie_" + extractHostname(currentUrl) + "_" + now_day + ".json";
+    a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    json = JSON.stringify(hash_cookie);
+    blob = new Blob([json], { type: "octet/stream" });
+    url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  };
+  document.getElementById("btnImportCookie").onchange = function () {
+    var reader = new FileReader();
+    var jsonObj = {};
+    reader.onload = function (event) {
+      Object.assign(jsonObj, JSON.parse(event.target.result));
+      loadObject(JSON.parse(event.target.result));
+    };
+    reader.readAsText(event.target.files[0]);
+  };
+});
